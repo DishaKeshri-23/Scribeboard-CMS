09976fab79e497f174f2d9d8b50345c32a78d4d9
Disha Keshri <dishakeshri23@gmail.com>
2026-03-01 14:49:26 +0530

    fix railway listen address

--- src/server.js
+++ src/server.js
@@ -27,8 +27,9 @@ app.get("/", (req, res) => {
 });
 // ----------- Start Server -------------
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 
